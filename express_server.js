6b8179455a425577e5f9c07fdae168edf1b2614e
MonajiLee <lee.smonica@gmail.com>
2018-10-03 06:16:36 +0000

    Added comments on Express properties and methods, and minor debugging

--- express_server.js
+++ express_server.js
@@ -23,39 +23,39 @@ app.get('/', (req, res) => {
 app.get('/urls', (req, res) => {
   let templateVars = { urls : urlDatabase };
   // MAGIC KEYWORD     ^key   ^key:value pairs of object defined above
-  res.render('urls_index', templateVars);
+  res.render('urls_index', templateVars);        // renders a view (/views/<>.ejs) with                                                          templateVars
 });
 
 app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+  res.json(urlDatabase);                         // sends parameter converted into .JSON string
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new");                        // renders a view (/views/<>.ejs) 
 });
 
 app.get('/urls/:id', (req, res) => {
   let templateVars = { shortURL: req.params.id,
                        longURL: urlDatabase[req.params.id] };
-  res.render("url_shows", templateVars);
+  res.render("url_shows", templateVars);        // renders a view (/views/<>.ejs) with                                                          templateVars
 });
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
-    res.redirect(longURL);
+    res.redirect(longURL);                      // redirects the specified URL path w/ status                                                 code
   });
 
 app.get('/hello', function(req, res){
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send("<html><body>Hello <b>World</b></body></html>\n");     // sends HTTP response
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
+  console.log(req.params);
   var random = generateRandomString();
   urlDatabase[random] = req.body.longURL;
-  res.redirect(`http://localhost:8080/urls/${random}`);
+  res.redirect(`http://localhost:8080/urls/${random}`);    // redirects the specified URL path                                                             w/ status code
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, () => {                        // starts a UNIX socket and listens for                                                           connections on the given path
   console.log(`Example app listening on port ${PORT}!`);
 });
