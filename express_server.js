c10f2b32f26626ff44b9362b35341dd629d472c6
MonajiLee <lee.smonica@gmail.com>
2018-10-03 02:56:44 +0000

    Day01 - Created Express server with two pages and one form to submit and record URLs in long form and short form

---
+++ express_server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 8080;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+let urlDatabase = {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com",
+};
+
+function generateRandomString() {
+  return Math.random().toString(16).substring(2, 8)
+};
+
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.get('/urls', (req, res) => {
+  let templateVars = { urls : urlDatabase };
+  // MAGIC KEYWORD     ^key   ^key:value pairs of object defined above
+  res.render('urls_index', templateVars);
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get('/urls/:id', (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+                       longURL: urlDatabase[req.params.id] };
+  res.render("url_shows", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+  });
+
+app.get('/hello', function(req, res){
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  var random = generateRandomString();
+  urlDatabase[random] = req.body.longURL;
+  res.redirect(`http://localhost:8080/urls/${random}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
