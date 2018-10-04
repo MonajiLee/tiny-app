5052a50ddb0089372a6a1f2cfa63d19848e2df62
MonajiLee <lee.smonica@gmail.com>
2018-10-04 01:04:11 +0000

    adding to cookie feature

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 let urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,7 +23,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls : urlDatabase };
+  let templateVars = { urls : urlDatabase,
+                       username: req.cookies["username"] };
   res.render('urls_index', templateVars);        // renders a view (/views/<>.ejs) with                                                          templateVars
 });
 
@@ -30,12 +33,15 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
+  let templateVars = { username: req.cookies["username"] };
   res.render("urls_new");                        // renders a view (/views/<>.ejs) 
 });
 
 app.get('/urls/:id', (req, res) => {
   let templateVars = { shortURL: req.params.id,
-                       longURL: urlDatabase[req.params.id] };
+                       longURL: urlDatabase[req.params.id],
+                       username: req.cookies["username"] };
+  console.log(req.cookies["username"]);
   res.render("url_shows", templateVars);        // renders a view (/views/<>.ejs) with                                                          templateVars
 });
 
@@ -62,6 +68,16 @@ app.post('/urls/:id/update', (req, res) => {
 
 app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id];
+  res.redirect('/urls');
+});
+
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username', req.body);
   res.redirect('/urls');
 });
 
